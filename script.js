9575e7c8f081236f6ec10864bc8dcc3bfbfb9638
Edwin <edwinpilat@gmail.com>
2020-01-08 07:16:29 +0100

    Lesson in progress

---
+++ script.js
@@ -0,0 +1,26 @@
+// alert('Wszystko Działa');
+
+//VARiable - zmienne
+var pojemnik = 'Witaj w Kursie JS ';
+var pojemnik2 = 20.334;
+
+var wiek = 30;
+// Alert to wyskakujące okno w przeglądarce
+// alert(pojemnik);
+
+// window.alert('Masz '+wiek+ ' lat');
+
+// Pokazywanie na konsoli
+console.log(pojemnik);
+
+//Tablica zmiennych
+var osoby = ['Jan Kowalski', 'Zbigniew S.', 'Aldona Nowak'];
+
+//Dodanie elementu na końcu tablicy
+osoby.push('Dariusz Flak');
+
+//Dodanie elementu na początku tablicy
+osoby.unshift('Na poczatku');
+
+//Wypisanie tablicy zmiennych w konsoli 
+console.log(osoby);
