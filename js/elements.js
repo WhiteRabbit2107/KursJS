525219a1a8bb85a785c1159174dbcb21ac075caf
Edwin <edwinpilat@gmail.com>
2020-01-17 07:52:53 +0100

    elements finished

--- js/elements.js
+++ js/elements.js
@@ -19,9 +19,9 @@ listaArtykulow.classList.add('text-center', 'text-uppercase');
 
 listaArtykulow.classList.remove('text-center');
 
-console.log(listaArtykulow.classList.contains('text-uppercase'));
-
-console.log(listaArtykulow);
+// console.log(listaArtykulow.classList.contains('text-uppercase'));
+//
+// console.log(listaArtykulow);
 
 //szukanie klas jumbotron wewnątrz listy artykkulów
 // listaArtykulow.getElementsByClassName('jumbotron');
@@ -37,3 +37,21 @@ tytul.innerHTML = '<p><b>Pogrubiony tekst</p></b>';
 // jeśli myslnik w css to w js camelCase
 tytul.style.color = '#df4531';
 tytul.style.marginTop = '102px';
+
+// wybieranie parenta/childa
+var el = document.querySelector('#lista-artykulow .jumbotron');
+console.log(el.parentNode);
+console.log(el.childNodes);
+console.log(el.firstChild);
+
+// dodoanie nowego elementu
+var nowyElementP = document.createElement('p');
+
+nowyElementP.classList.add('text-center', 'text-uppercase');
+nowyElementP.style.color = 'red';
+nowyElementP.innerHTML = '<b>lorem ipsum</b> dolor sit ament';
+
+document.body.appendChild(nowyElementP);
+
+// usuwanie elementu - trzeba sie odniesc do rodzica
+nowyElementP.parentNode.removeChild(nowyElementP);
