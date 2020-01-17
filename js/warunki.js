747863b847b0ffab182666f9a40fd6bbc78ea065
Edwin <edwinpilat@gmail.com>
2020-01-17 08:01:52 +0100

    if started

---
+++ js/warunki.js
@@ -0,0 +1,14 @@
+var wiek = 23;
+
+if (wiek == 18){
+  alert('Masz 18 lat');
+}
+else if (wiek > 18) {
+  alert('Masz więcej niż 18 lat');
+}
+else if (wiek <= 16) {
+  alert('Masz mniej lub rowno 16 lat');
+}
+else {
+  alert('Nie rozpoznałem Twojego wieku');
+}
